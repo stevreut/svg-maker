d236f2c81d7c2e1b2277c561bb69be05159e5b94
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-18 22:02:54 -0400

    unit test render() in all shapes, add text.js and unit test

---
+++ temp_test.js
@@ -0,0 +1,20 @@
+const Circle = require('./lib/circle.js');
+const Triangle = require('./lib/triangle.js');
+const Square = require('./lib/square.js');
+const Text = require('./lib/text.js');
+
+let circle = new Circle("95.5","16.2","red","5.0");
+let rndr = circle.render();
+console.log('circle rendered = ' + rndr);
+
+let square = new Square("55",30,"blue","20.03");
+rndr = square.render();
+console.log('sqr rendered = ' + rndr);
+
+let tri = new Triangle("55",30,"#303035",20.);
+rndr = tri.render();
+console.log('tri rendered = ' + rndr);
+
+let txt = new Text("40",35,"cyan","Hello");
+rndr = txt.render();
+console.log('txt = ' + rndr);
