991a86e3d3243ca7c871e8f76368074e30c7de45
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-19 19:24:35 -0400

    color and initials enforcements added, file name logging added

--- index.js
+++ index.js
@@ -25,27 +25,73 @@ let quests = [
     }
 ];
 
+let outFileName = 'logo.svg';
+
+function validHexColor(color) {
+    if (color === undefined || color === null || color.trim() === '') {
+        return false;
+    }
+    if (color.length != 7 && color.length != 4) {
+        return false;
+    }
+    if (color.charAt(0) !== '#') {
+        return false;
+    }
+    for (let i=1;i<color.length;i++) {
+        let ch = color.charAt(i);
+        if (!((ch >= 'A' && ch <= 'F') ||
+              (ch >= 'a' && ch <= 'f') ||
+              (ch >= '0' && ch <= '9'))) {
+                return false;
+        }
+    }
+    return true;
+}
+
+function validateColor(color, colorLabel) {
+    color = color.trim().toLowerCase();
+    if (color.length < 3) {
+        console.log('\n\nInvalid color ' + color + ' on ' + colorLabel + ' replaced with black\n');
+        color = 'black';
+        return color;
+    } else if (color.charAt(0) === '#') {
+        if (!validHexColor(color)) {
+            console.log('\n\nInvalid hex color ' + color + ' on ' + colorLabel + ' replaced with black\n');
+            color = 'black';
+            return color;
+        } else {
+            return color;
+        }
+    } else {
+        return color;  // TODO - more validation would be nice
+    }
+}
+
 inquirer.prompt(quests).then((answers) => {
     console.log('answers = "' + answers + '"');
     console.log('answers json = "' + JSON.stringify(answers) + '"');
     return answers;
 }).then((answers) => {
     console.log('answers json after then = "' + JSON.stringify(answers) + '"');
-    // TODO - normalize answers
-    let outFileName = './logo.svg';
-    let nonsenseContent = 'line 1';  // TODO
-    nonsenseContent += '\nline 2';
-    let svg = new SVG(answers.inits,answers.textColor,answers.shape,answers.shapeColor);
+    let { inits, textColor, shape, shapeColor } = answers;
+    inits = inits.trim().toUpperCase();
+    if (inits.length === 0) {
+        console.log('\n\nNo initials entered, defaulting to XYZ\n');
+        inits = 'XYZ';
+    } else if (inits.length > 3) {
+        console.log('\n\nToo many initials entered, will be truncated\n');
+        inits = inits.substring(0,3);
+    }
+    textColor = validateColor(textColor, 'text color');
+    shapeColor = validateColor(shapeColor, 'shape color');
+    let svg = new SVG(inits,textColor,shape,shapeColor);
     let svgStr = svg.render();
     console.log('svg rendered = "' + svgStr + '"');
     return fs.writeFile(outFileName,svgStr);
 }).then((writePromise) => {
-    console.log('prompt kept');
-    console.log('presumably logo.svg written');
+    console.log('\n\n   Generated ' + outFileName + '\n\n');
 }).catch((error) => {
     if (error) {
-        console.log('error caught = "' + error + '"');
-    } else {
-        console.log('processing complete in catch');
+        console.log('error encountered = "' + error + '"');
     }
 });
